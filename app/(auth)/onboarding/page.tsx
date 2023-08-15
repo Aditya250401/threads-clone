37822c1c5db2e34b4c15079236499b4843b52347
Aditya250401 <aa.21u10091@btech.nitdgp.ac.in>
2023-08-15 09:50:43 +0530

    addition of bars

---
+++ app/(auth)/onboarding/page.tsx
@@ -0,0 +1,9 @@
+async function page() {
+	return (
+		<div>
+			<h1 className="head-text">Onboarding</h1>
+		</div>
+	)
+}
+
+export default page
---
+++ app/(root)/page.tsx
@@ -0,0 +1,11 @@
+//app/page.tss
+
+export default function Home() {
+	return (
+		<>
+			<h1 className="head-text text-left">
+				Hello i making one of the best websites possible
+			</h1>
+		</>
+	)
+}
